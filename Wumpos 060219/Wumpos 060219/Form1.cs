726b19908266510166d822f1b7ee30737fbced0e
DESKTOP-DIRTT1E\Cazadorable <tyson.j.gerlach@gmail.com>
2019-02-08 16:25:47 +0800

    NuGet Packages installed for RESTSharp and API from client to php installed (no serialization)

--- Wumpos 060219/Wumpos 060219/Form1.cs
+++ Wumpos 060219/Wumpos 060219/Form1.cs
@@ -20,6 +20,9 @@ namespace Wumpos_060219
 
         string path = "file.txt";
         FileHandler file_handler = new FileHandler();
+        DataHandler data_handler = new DataHandler();
+
+
         public static String GetTimestamp(DateTime value)
         {
             return value.ToString("yyyyMMddHHmmssffff");
@@ -29,66 +32,100 @@ namespace Wumpos_060219
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 0, timeStamp);
+            string name = "john";
+            string score = "0";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_1_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 1, timeStamp);
+            string name = "john";
+            string score = "1";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_2_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 2, timeStamp);
+            string name = "john";
+            string score = "2";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_3_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 3, timeStamp);
+            string name = "john";
+            string score = "3";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_4_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 4, timeStamp);
+            string name = "john";
+            string score = "4";
+            data_handler.sendToApi(name, score, timeStamp);
+
         }
 
         private void Button_5_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 5, timeStamp);
+            string name = "john";
+            string score = "5";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_6_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 6, timeStamp);
+            string name = "john";
+            string score = "6";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_7_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 7, timeStamp);
+            string name = "john";
+            string score = "7";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_8_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 8, timeStamp);
+            string name = "john";
+            string score = "8";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_9_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 9, timeStamp);
+            string name = "john";
+            string score = "9";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Button_10_Click(object sender, EventArgs e)
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             file_handler.openFileAndWriteNumber(path, 10, timeStamp);
+            string name = "john";
+            string score = "10";
+            data_handler.sendToApi(name, score, timeStamp);
         }
 
         private void Form1_Load(object sender, EventArgs e)
