040cdd7985486dcaf2b7aa8c880a5e4374895115
Wompus123 <jeanlucm@our.ecu.edu.au>
2019-03-16 15:32:25 +0800

    New perspectives

---
+++ New Perspectives/DataHandler.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+
+namespace New_Perspectives
+{
+    class DataHandler
+    {
+        private const string URL = "http://www.wumpos.com";
+
+        public bool sendToApi(string name, string score, string timeStamp)
+        {
+
+            var client = new RestClient(URL);
+
+            var request = new RestRequest("/api/saveData.php", Method.POST);
+            request.AddParameter("name", name); // adds to POST or URL querystring based on Method
+            request.AddParameter("score", score);
+            request.AddParameter("timestamp", timeStamp);
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            return true;
+        }
+    }
+}
---
+++ New Perspectives/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace New_Perspectives
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.aButton_1 = new System.Windows.Forms.Button();
+            this.aWelcomeLabel = new System.Windows.Forms.Label();
+            this.aButton_10 = new System.Windows.Forms.Button();
+            this.aButton_9 = new System.Windows.Forms.Button();
+            this.aButton_8 = new System.Windows.Forms.Button();
+            this.aButton_7 = new System.Windows.Forms.Button();
+            this.aButton_6 = new System.Windows.Forms.Button();
+            this.aButton_5 = new System.Windows.Forms.Button();
+            this.aButton_4 = new System.Windows.Forms.Button();
+            this.aButton_3 = new System.Windows.Forms.Button();
+            this.aButton_2 = new System.Windows.Forms.Button();
+            this.aMaximiseScreenTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // aButton_1
+            // 
+            this.aButton_1.Location = new System.Drawing.Point(147, 17);
+            this.aButton_1.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_1.Name = "aButton_1";
+            this.aButton_1.Size = new System.Drawing.Size(100, 28);
+            this.aButton_1.TabIndex = 24;
+            this.aButton_1.Text = "1";
+            this.aButton_1.UseVisualStyleBackColor = true;
+            this.aButton_1.Click += new System.EventHandler(this.aButton_1_Click);
+            // 
+            // aWelcomeLabel
+            // 
+            this.aWelcomeLabel.AutoSize = true;
+            this.aWelcomeLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aWelcomeLabel.Location = new System.Drawing.Point(12, 17);
+            this.aWelcomeLabel.Name = "aWelcomeLabel";
+            this.aWelcomeLabel.Size = new System.Drawing.Size(121, 46);
+            this.aWelcomeLabel.TabIndex = 23;
+            this.aWelcomeLabel.Text = "How are you\r\nFeeling? ";
+            // 
+            // aButton_10
+            // 
+            this.aButton_10.Location = new System.Drawing.Point(147, 341);
+            this.aButton_10.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_10.Name = "aButton_10";
+            this.aButton_10.Size = new System.Drawing.Size(100, 28);
+            this.aButton_10.TabIndex = 22;
+            this.aButton_10.Text = "10";
+            this.aButton_10.UseVisualStyleBackColor = true;
+            this.aButton_10.Click += new System.EventHandler(this.aButton_10_Click);
+            // 
+            // aButton_9
+            // 
+            this.aButton_9.Location = new System.Drawing.Point(147, 305);
+            this.aButton_9.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_9.Name = "aButton_9";
+            this.aButton_9.Size = new System.Drawing.Size(100, 28);
+            this.aButton_9.TabIndex = 21;
+            this.aButton_9.Text = "9";
+            this.aButton_9.UseVisualStyleBackColor = true;
+            this.aButton_9.Click += new System.EventHandler(this.aButton_9_Click);
+            // 
+            // aButton_8
+            // 
+            this.aButton_8.Location = new System.Drawing.Point(147, 269);
+            this.aButton_8.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_8.Name = "aButton_8";
+            this.aButton_8.Size = new System.Drawing.Size(100, 28);
+            this.aButton_8.TabIndex = 20;
+            this.aButton_8.Text = "8";
+            this.aButton_8.UseVisualStyleBackColor = true;
+            this.aButton_8.Click += new System.EventHandler(this.aButton_8_Click);
+            // 
+            // aButton_7
+            // 
+            this.aButton_7.Location = new System.Drawing.Point(147, 233);
+            this.aButton_7.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_7.Name = "aButton_7";
+            this.aButton_7.Size = new System.Drawing.Size(100, 28);
+            this.aButton_7.TabIndex = 19;
+            this.aButton_7.Text = "7";
+            this.aButton_7.UseVisualStyleBackColor = true;
+            this.aButton_7.Click += new System.EventHandler(this.aButton_7_Click);
+            // 
+            // aButton_6
+            // 
+            this.aButton_6.Location = new System.Drawing.Point(147, 197);
+            this.aButton_6.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_6.Name = "aButton_6";
+            this.aButton_6.Size = new System.Drawing.Size(100, 28);
+            this.aButton_6.TabIndex = 14;
+            this.aButton_6.Text = "6";
+            this.aButton_6.UseVisualStyleBackColor = true;
+            this.aButton_6.Click += new System.EventHandler(this.aButton_6_Click);
+            // 
+            // aButton_5
+            // 
+            this.aButton_5.Location = new System.Drawing.Point(147, 161);
+            this.aButton_5.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_5.Name = "aButton_5";
+            this.aButton_5.Size = new System.Drawing.Size(100, 28);
+            this.aButton_5.TabIndex = 18;
+            this.aButton_5.Text = "5";
+            this.aButton_5.UseVisualStyleBackColor = true;
+            this.aButton_5.Click += new System.EventHandler(this.aButton_5_Click);
+            // 
+            // aButton_4
+            // 
+            this.aButton_4.Location = new System.Drawing.Point(147, 125);
+            this.aButton_4.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_4.Name = "aButton_4";
+            this.aButton_4.Size = new System.Drawing.Size(100, 28);
+            this.aButton_4.TabIndex = 17;
+            this.aButton_4.Text = "4";
+            this.aButton_4.UseVisualStyleBackColor = true;
+            this.aButton_4.Click += new System.EventHandler(this.aButton_4_Click);
+            // 
+            // aButton_3
+            // 
+            this.aButton_3.Location = new System.Drawing.Point(147, 89);
+            this.aButton_3.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_3.Name = "aButton_3";
+            this.aButton_3.Size = new System.Drawing.Size(100, 28);
+            this.aButton_3.TabIndex = 16;
+            this.aButton_3.Text = "3";
+            this.aButton_3.UseVisualStyleBackColor = true;
+            this.aButton_3.Click += new System.EventHandler(this.aButton_3_Click);
+            // 
+            // aButton_2
+            // 
+            this.aButton_2.Location = new System.Drawing.Point(147, 53);
+            this.aButton_2.Margin = new System.Windows.Forms.Padding(4);
+            this.aButton_2.Name = "aButton_2";
+            this.aButton_2.Size = new System.Drawing.Size(100, 28);
+            this.aButton_2.TabIndex = 15;
+            this.aButton_2.Text = "2";
+            this.aButton_2.UseVisualStyleBackColor = true;
+            this.aButton_2.Click += new System.EventHandler(this.aButton_2_Click);
+            // 
+            // aMaximiseScreenTimer
+            // 
+            this.aMaximiseScreenTimer.Enabled = true;
+            this.aMaximiseScreenTimer.Interval = 3600000;
+            this.aMaximiseScreenTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(260, 380);
+            this.Controls.Add(this.aButton_1);
+            this.Controls.Add(this.aWelcomeLabel);
+            this.Controls.Add(this.aButton_10);
+            this.Controls.Add(this.aButton_9);
+            this.Controls.Add(this.aButton_8);
+            this.Controls.Add(this.aButton_7);
+            this.Controls.Add(this.aButton_6);
+            this.Controls.Add(this.aButton_5);
+            this.Controls.Add(this.aButton_4);
+            this.Controls.Add(this.aButton_3);
+            this.Controls.Add(this.aButton_2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "New Perspectives";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button aButton_1;
+        private System.Windows.Forms.Label aWelcomeLabel;
+        private System.Windows.Forms.Button aButton_10;
+        private System.Windows.Forms.Button aButton_9;
+        private System.Windows.Forms.Button aButton_8;
+        private System.Windows.Forms.Button aButton_7;
+        private System.Windows.Forms.Button aButton_6;
+        private System.Windows.Forms.Button aButton_5;
+        private System.Windows.Forms.Button aButton_4;
+        private System.Windows.Forms.Button aButton_3;
+        private System.Windows.Forms.Button aButton_2;
+        private System.Windows.Forms.Timer aMaximiseScreenTimer;
+    }
+}
+
